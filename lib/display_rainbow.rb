5f1bfa7bd4fa080faee53c9db175c8ba4b7f7330
henry rahmawan <rahmawan.henry@gmail.com>
2021-06-17 21:00:20 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,5 +6,5 @@ def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indig
   puts "G: #{colors[3]}, "
   puts "B: #{colors[4]}, "
   puts "I: #{colors[5]}, "
-  puts "V: #{colors[6]}\n"
+  puts "V: #{colors[6]}"
 end
